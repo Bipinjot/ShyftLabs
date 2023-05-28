cf6cf879dba9e5e4163c1c4322c9d72b843e573b
Bipinjot Kaur Hara <bkhara@uwaterloo.ca>
2023-05-28 09:27:05 -0400

    Home Page setup

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { IoMdSchool } from "react-icons/io";
 
 function Home() {
   return (
     <>
-      <div>
+      <div className="flex flex-col w-[1000px] justify-center items-center">
+      <h1 className="text-5xl font-bold mb-10 text-slate-500">Student</h1>
+      <h1 className="text-5xl font-bold mb-10 text-slate-500">Management</h1>
+      <h1 className="text-5xl font-bold mb-10 text-slate-500">System</h1>
+      <IoMdSchool className="text-7xl text-slate-800" />
       </div>
     </>
   );
