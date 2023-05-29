b6d913ebfe8ef40e1993c93e6498da84884ae406
Bipinjot Kaur Hara <bkhara@uwaterloo.ca>
2023-05-29 13:45:34 -0400

    Filter on results

--- frontend/src/Pages/Results.js
+++ frontend/src/Pages/Results.js
@@ -3,16 +3,90 @@ import axios from "axios";
 
 function Results() {
   let [resultList, setResultList] = useState([]);
+  const [courses, setCourses] = useState([]);
+  const [students, setStudents] = useState([]);
+
+  // useEffect(() => {
+  //   // Fetch courses from API
+  //   axios
+  //     .get("http://127.0.0.1:8000//allcourse/")
+  //     .then((res) => {
+  //       setCourses(res.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+
+  //   // Fetch students from API
+  //   axios
+  //     .get("http://127.0.0.1:8000//alluser/")
+  //     .then((res) => {
+  //       setStudents(res.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+
+  //   // Fetch results from API
+    // axios
+    //   .get("http://127.0.0.1:8000/allresult/")
+    //   .then((res) => {
+    //     const filteredResults = res.data.filter((result) => {
+    //       // Filter based on courses and students
+    //       return (
+    //         courses.includes(result.shyft_resultcourse) &&
+    //         students.includes(result.shyft_resultuser)
+    //       );
+    //     });
+    //     setResultList(filteredResults);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+  // }, [courses, students]);
+
   useEffect(() => {
     //console.log("check");
     axios
       .get("http://127.0.0.1:8000/allresult/")
       .then((res) => {
+        // console.log("resultBeforeFilter",resultList)
+        const filteredResults = res.data.filter((result) => {
+          return (
+            courses.some((course) => course.shyft_coursename === result.shyft_resultcourse)
+            &&
+            students.some((student) => (student.shyft_name) === result.shyft_resultuser)
+            )
+        });
+        setResultList(filteredResults);
+      });
+    
+  }, [courses, students]);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000//alluser/")
+      .then((res) => {
         console.log(res.data);
-        setResultList(res.data);
+        setStudents(res.data);
       });
     
   }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000//allcourse/")
+      .then((res) => {
+        // console.log(res.data);
+        setCourses(res.data);
+      });
+    
+  }, []);
+
+  // console.log("result",resultList)
+  // console.log("Student",students)
+  // console.log("Courses",courses)
+
   const columns = [
     { Header: "Course Name", accessor: "shyft_resultcourse" },
     { Header: "Student Name", accessor: "shyft_resultuser" },
